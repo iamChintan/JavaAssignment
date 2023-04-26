50c2e9be70a5acf8802c6ff882b4a1da9718a9ab
iamChintan <88285492+iamChintan@users.noreply.github.com>
2023-04-25 22:50:38 -0400

    Task 3 Problem 2

--- AssignmentTask3.java
+++ AssignmentTask3.java
@@ -6,7 +6,7 @@ public class AssignmentTask3 {
         Scanner input = new Scanner(System.in);
 
         // Problem 1: FibonacciSeries
-        System.out.println("Please Enter the number : ");
+        System.out.println("Please Enter the number for Fibonacci : ");
         int num = input.nextInt();
 
         int prevNum = 0, currNum = 1, nextNum;
@@ -16,6 +16,22 @@ public class AssignmentTask3 {
             prevNum = currNum;
             currNum = nextNum;
         }
+
+        System.out.println(" ");
+
+        // problem 2: Factorial
+        System.out.println("Please Enter the number for Factorial : ");
+        int fact = input.nextInt();
+        int factorial = 1;
+
+        for (int i = 1; i <= fact; i++){
+            factorial *= i;
+        }
+        System.out.println(" factorial is "  + factorial);
+        System.out.println(" ");
+
+
+        // problem 3: Check Palindrome.
 
     }
 }
