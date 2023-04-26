ca2cec040cce3fec0302fe1d1dfe9b83986ad294
iamChintan <88285492+iamChintan@users.noreply.github.com>
2023-04-25 22:44:47 -0400

    Task 3 Problem 1

--- AssignmentTask3.java
+++ AssignmentTask3.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class AssignmentTask3 {
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
+        // Problem 1: FibonacciSeries
+        System.out.println("Please Enter the number : ");
+        int num = input.nextInt();
+
+        int prevNum = 0, currNum = 1, nextNum;
+        for (int i = 0; i <= num; i++){
+            System.out.print(prevNum + " ");
+            nextNum = prevNum + currNum;
+            prevNum = currNum;
+            currNum = nextNum;
+        }
+
     }
 }
